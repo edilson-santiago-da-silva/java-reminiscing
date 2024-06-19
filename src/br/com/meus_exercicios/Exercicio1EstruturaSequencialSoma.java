f68bd9f435d52510d7ab74bb49ca8775497b7bb6
Edilson Santiago <edilsonsantiagoe7@gmail.com>
2024-06-19 09:47:42 -0300

    excercicio1SequencialSoma

---
+++ src/br/com/meus_exercicios/Exercicio1EstruturaSequencialSoma.java
@@ -0,0 +1,21 @@
+package br.com.meus_exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio1EstruturaSequencialSoma {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número:" );
+        int n1 = sc.nextInt();
+
+        System.out.println("Digite o segundo número:");
+        int n2 = sc.nextInt();
+
+        int soma = n1 + n2;
+        System.out.println("A soma é " + soma);
+
+        sc.close();
+    }
+}
