5dad9ed25c97ebe68ebbde267edee82e2966ec59
Edilson Santiago <edilsonsantiagoe7@gmail.com>
2024-06-20 16:36:58 -0300

    Exercicio4EstruturaRepetitivaLoopWhileNumerosInteirosSoma

---
+++ src/br/com/meus_exercicios/Exercicio4EstruturaRepetitivaLoopWhileNumerosInteirosSoma.java
@@ -0,0 +1,23 @@
+package br.com.meus_exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio4EstruturaRepetitivaLoopWhileNumerosInteirosSoma {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite números inteiros no loop, quando quiser teminar o loop digite zero e verá a soma dos números digitados.");
+        int n = 1;
+        int cont = 1;
+        int soma = 0;
+        while (n != 0){
+            System.out.print(cont + "° número: ");
+            n = sc.nextInt();
+            soma += n;
+            cont += 1;
+        }
+        System.out.println("A soma dos números digitados foi: " + soma);
+        sc.close();
+    }
+}
