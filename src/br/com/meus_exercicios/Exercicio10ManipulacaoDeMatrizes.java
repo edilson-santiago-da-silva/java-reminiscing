28184fcf14c138242771bb52e96060f25e68eccb
Edilson Santiago <edilsonsantiagoe7@gmail.com>
2024-06-27 10:00:03 -0300

    Exercicio10ManipulacaoDeMatrizes

---
+++ src/br/com/meus_exercicios/Exercicio10ManipulacaoDeMatrizes.java
@@ -0,0 +1,24 @@
+package br.com.meus_exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio10ManipulacaoDeMatrizes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int [][] matriz = new int[3][3];
+
+        for (int i = 0; i < 3; i++ ) {
+            for (int j = 0; j < 3; j++){
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < 3; i++ ) {
+            for (int j = 0; j < 3; j++){
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
