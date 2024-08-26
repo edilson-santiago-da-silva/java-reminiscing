5729d480e69bdbfb920538fba66747df048ee092
edilson-santiago-da-slva <edilsonsantiagoe7@gmail.com>
2024-08-26 11:42:44 -0300

    Exercicio12DescVetores2

---
+++ src/br/com/meus_exercicios/Exercicio12DescomplicaVetoresSoma.java
@@ -0,0 +1,24 @@
+package br.com.meus_exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio12DescomplicaVetoresSoma {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int [] vector = new int[50];
+        int media, soma = 0;
+
+        for (int i = 0; i <= 49; i ++ ){
+            System.out.print("Digite um número? ");
+            vector[i] = sc.nextInt();
+            soma += vector[i];
+        }
+
+        media = soma / 50;
+
+
+        System.out.println("A média das 50 notas digitadas é: " + media);
+        sc.close();
+    }
+}
